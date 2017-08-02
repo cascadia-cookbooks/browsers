b7655c57450535f41a042800c41e97b743cd12b5
Colby Olson <me@colbyolson.com>
2017-08-02 15:35:41 -0700

    use minor versions

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'engineering@copiousinc.com'
 license 'MIT'
 description 'Installs and configures different browsers.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.0.1'
+version '0.1.0'
 
 source_url 'https://github.com/copious-cookbooks/browsers'
 issues_url 'https://github.com/copious-cookbooks/browsers/issues'
